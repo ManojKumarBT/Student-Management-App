c8022468be45c74f0c4df54a51567c51eadbba14
Manoj Kumar B T <70774620+ManojKumarBT@users.noreply.github.com>
2025-02-22 22:53:01 +0530

    Added a Comment

--- BackEnd/src/main/java/com/syfassignment/studentmanagementapp/exception/InvalidInputException.java
+++ BackEnd/src/main/java/com/syfassignment/studentmanagementapp/exception/InvalidInputException.java
@@ -1,5 +1,6 @@
 package com.syfassignment.studentmanagementapp.exception;
 
+//Exception if the user sends Invalid Input
 public class InvalidInputException extends RuntimeException {
     public InvalidInputException(String message) {
         super(message);
