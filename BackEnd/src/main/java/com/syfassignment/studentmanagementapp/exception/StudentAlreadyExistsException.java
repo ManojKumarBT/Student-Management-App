15d93e2ee3a884cc410e437f2412d6c7302fb3b7
Manoj Kumar B T <70774620+ManojKumarBT@users.noreply.github.com>
2025-02-22 22:52:15 +0530

    Added a Comment

--- BackEnd/src/main/java/com/syfassignment/studentmanagementapp/exception/StudentAlreadyExistsException.java
+++ BackEnd/src/main/java/com/syfassignment/studentmanagementapp/exception/StudentAlreadyExistsException.java
@@ -1,5 +1,6 @@
 package com.syfassignment.studentmanagementapp.exception;
 
+//Exception if Student Data already present in the DB
 public class StudentAlreadyExistsException extends RuntimeException {
     public StudentAlreadyExistsException(String message) {
         super(message);
