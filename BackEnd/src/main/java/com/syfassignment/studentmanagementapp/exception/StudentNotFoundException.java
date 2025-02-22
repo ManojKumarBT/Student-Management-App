71c9738b8e30d104b6ffb3b6658077b51713523c
Manoj Kumar B T <70774620+ManojKumarBT@users.noreply.github.com>
2025-02-22 22:51:03 +0530

    Added a Comment

--- BackEnd/src/main/java/com/syfassignment/studentmanagementapp/exception/StudentNotFoundException.java
+++ BackEnd/src/main/java/com/syfassignment/studentmanagementapp/exception/StudentNotFoundException.java
@@ -1,5 +1,6 @@
 package com.syfassignment.studentmanagementapp.exception;
 
+//Exception if Student data is not present
 public class StudentNotFoundException extends RuntimeException {
     public StudentNotFoundException(String message) {
         super(message);
